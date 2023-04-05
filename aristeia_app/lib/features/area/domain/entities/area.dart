bb216fdb31bf93ae14100716a51f66448e05d8ca
juan <jcortesj@unal.edu.co>
2023-04-05 16:51:21 -0500

    agregacion cloud_firestore + etiqueta + area +usuario

---
+++ aristeia_app/lib/features/area/domain/entities/area.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+class Area {
+  late String nombre;
+  late List<Tema> subtemas;
+  late String descripcion;
+
+  Area(this.nombre, this.descripcion) {
+    this.subtemas = [];
+  }
+
+  void addSubtemas(Tema tema) {
+    this.subtemas.add(tema);
+  }
+}
