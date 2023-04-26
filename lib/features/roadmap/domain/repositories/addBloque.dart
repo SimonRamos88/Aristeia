54ffa28f5231d7c4b497d9a526a4ed966c22beca
jaim3s <jujaimesc@unal.edu.co>
2023-04-25 23:25:04 -0500

    CRUD bloque excepto Get

---
+++ lib/features/roadmap/domain/repositories/addBloque.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+Future<void> addBlock(String roadmapID, String title, String description, int importance, DateTime startDate, DateTime endDate) async {
+  // Instanciar la db
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  // Acceder a la colección y al documento
+  await db.collection('roadmap').doc(roadmapID).collection('bloques').add({
+      "titulo": title, 
+      "descripcion": description, 
+      "completado": false, 
+      "importancia": importance, 
+      "fechaInicio": startDate, 
+      "fechaFin": endDate
+    });
+}
---
+++ lib/features/roadmap/domain/repositories/deleteBloque.dart
@@ -0,0 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+Future<void> deleteBlock(String? roadmapID, String? blockID) async {
+  // Instanciar la db
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  // Acceder a la colección y al documento
+  await db.collection("roadmap").doc(roadmapID).collection('bloques').doc(blockID).delete();
+}
