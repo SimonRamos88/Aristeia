3e89760d00dcf1670b17aa065fe60ab2abc15520
SimonRamos88 <joramos@unal.edu.co>
2023-05-06 18:23:45 -0500

    create recurso implemented

--- lib/features/Recurso/domain/repositories/addRecurso.dart
+++ lib/features/Recurso/domain/repositories/addRecurso.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 Future<String> createRecurso(
-    Map<String, dynamic> data, String roadmapID, String bloqueID) async {
+    Map<String, dynamic> data, String roadmapID, String bloqueID, String recursoID) async {
   String res = "";
   FirebaseFirestore db = FirebaseFirestore.instance;
   CollectionReference collectionReferenceRecurso = db
@@ -19,11 +19,58 @@ Future<String> createRecurso(
       data.containsKey("autor") &&
       data.containsKey("imagen")) {
     //subimos los datos del evento
-    await collectionReferenceRecurso.add(data);
+    await collectionReferenceRecurso.doc(recursoID).set(data);
     res = 'datos subidos con exito';
   } else {
     res = 'datos insuficientes';
   }
 
   return res;
+}
+
+
+Future<int> getRecursoAmount( String roadmapID, String bloqueID) async{
+
+  int amount = -1;
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  CollectionReference collectionReferenceRecurso = db
+      .collection('roadmap')
+      .doc(roadmapID)
+      .collection('bloques')
+      .doc(bloqueID)
+      .collection('recursos');
+  
+  QuerySnapshot query= await collectionReferenceRecurso.get();
+  if(!query.docs.isEmpty){
+    amount = query.size;
+  }
+  
+  return amount;
+
+}
+
+
+List<String> StringToList (String input) {
+  
+  List<String> list = [];
+  String cad = "";
+  
+  input.runes.forEach((e) {
+    var ch = String.fromCharCode(e);
+    if(ch!=" "){
+        cad += ch;
+    }else{
+      if(cad != ""){
+        list.add(cad);
+        cad = "";
+      }else{
+        cad = "";
+      }
+    }
+  });
+
+  if(cad!=""){
+    list.add(cad);
+  }
+  return list;
 }
