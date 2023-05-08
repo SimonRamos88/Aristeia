fe0e2c88ae7e58d208c1fb5636ff463075650ea9
SimonRamos88 <joramos@unal.edu.co>
2023-05-07 20:48:37 -0500

    editar recurso implementado

--- lib/features/roadmap/presentation/pages/create_resource_screen.dart
+++ lib/features/roadmap/presentation/pages/create_resource_screen.dart
@@ -9,6 +9,9 @@ import 'package:aristeia_app/core/widgets/app_bar_widget.dart';
 import 'package:aristeia_app/core/widgets/input_field.dart';
 import 'package:aristeia_app/core/widgets/resource_card.dart';
 import 'package:aristeia_app/features/Recurso/domain/repositories/addRecurso.dart';
+import 'package:aristeia_app/features/Recurso/domain/repositories/getAllRecursos.dart';
+import 'package:aristeia_app/features/Recurso/domain/repositories/getRecurso.dart';
+import 'package:aristeia_app/features/recurso/domain/repositories/updateRecurso.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -34,10 +37,7 @@ class CreateResourceScreen extends StatefulWidget {
 
 class _CreateResourceScreenState extends State<CreateResourceScreen> {
   final colors = AppColors();
-  final TextEditingController nombreController = TextEditingController();
-  final TextEditingController descripcionController = TextEditingController();
-  final TextEditingController linksController = TextEditingController();
-
+  
   // list of tiles
   final List myTiles = [
     'A',
@@ -45,6 +45,9 @@ class _CreateResourceScreenState extends State<CreateResourceScreen> {
     'C',
     'D',
   ];
+
+  final Map<int, dynamic> recursos = {};
+
 
   Map<String, dynamic> bloqueCreado = {};
 
@@ -81,6 +84,10 @@ class _CreateResourceScreenState extends State<CreateResourceScreen> {
   }
 
   void agregarRecurso(){
+    final TextEditingController nombreController = TextEditingController();
+    final TextEditingController descripcionController = TextEditingController();
+    final TextEditingController linksController = TextEditingController();
+
     showDialog(
       context: context,
       builder: (BuildContext context) => SingleChildScrollView(
@@ -117,7 +124,17 @@ class _CreateResourceScreenState extends State<CreateResourceScreen> {
                           'descripcion': descripcionController.text,
                           'links_relacionados':StringToList(linksController.text),
                           'autor': 'autorX',
-                          'imagen': '123'}, widget.roadId.toString(), widget.blockId.toString(), recursoId.toString());    
+                          'imagen': '123'}, widget.roadId.toString(), widget.blockId.toString(), recursoId.toString());
+            
+                
+            setState((){
+              //myTiles.add(nombreController.text);
+              recursos[recursoId] = nombreController.text;
+            });     
+            nombreController.clear();
+            descripcionController.clear();
+            linksController.clear(); 
+            log(recursos.toString());         
             Navigator.of(context).pop();
           },
           onTapLeft: () {
@@ -127,6 +144,86 @@ class _CreateResourceScreenState extends State<CreateResourceScreen> {
       ),
     );
   }
+
+
+  getListaRecursos() async{
+    List listaRecursos = await getRecursos(widget.roadId.toString(), widget.blockId.toString());
+    for(final e in listaRecursos){
+      //log("log: " + e['nombre'] + e.id);
+      int keyR = int.parse(e.id); 
+      recursos[keyR] = e['nombre'];
+    }
+
+  }
+
+  void editarRecurso(int recursoId) async{
+
+    final TextEditingController nombreController = TextEditingController();
+    final TextEditingController descripcionController = TextEditingController();
+    final TextEditingController linksController = TextEditingController();
+
+    Map<String, dynamic> recurso = await getRecurso(widget.roadId.toString(), widget.blockId.toString(), recursoId.toString()); 
+    
+    nombreController.text = recurso['nombre'];
+    descripcionController.text = recurso['descripcion'];
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => SingleChildScrollView(
+        child: AlertDialogWidget(
+          insetPadding: true,
+            width: MediaQuery.of(context).size.width,
+          color: 2,
+          tituloGeneral: false,
+          tituloPersonalizado: Text(
+            'Editar recurso',
+            style: heading2bStyle.copyWith(color: colors.pinkColor),
+            textAlign: TextAlign.center,
+          ),
+          more: Column(
+            children: [
+              InputField(
+                  hintText: 'Nombre recurso',
+                  controller: nombreController),
+              InputField(
+                  hintText: 'Descripción',
+                  maxLines: 3,
+                  controller: descripcionController),
+              InputField(
+                  hintText: 'Links',
+                  maxLines: 2,
+                  controller: linksController),
+            ],
+          ),
+          rightText: 'Editar',
+          leftText: 'Cancelar',
+          onTapRight: () async {
+            
+            updateRecurso(widget.roadId.toString(), widget.blockId.toString(), recursoId.toString(), 
+            {'nombre' : nombreController.text,
+            'descripcion': descripcionController.text,
+            'links_relacionados':StringToList(linksController.text),
+            'autor': 'autorX',
+            'imagen': '123'});
+
+            setState((){
+              recursos[recursoId] = nombreController.text;
+            });
+            nombreController.clear();
+            descripcionController.clear();
+            linksController.clear();                
+            Navigator.of(context).pop();
+          },
+          onTapLeft: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+    );
+  }
+
+  
+
 
   void borrarRecurso() {
     showDialog(
@@ -150,6 +247,7 @@ class _CreateResourceScreenState extends State<CreateResourceScreen> {
   void initState() {
     // TODO: implement initState
     traerBloque(widget.roadId.toString(), widget.blockId.toString());
+    getListaRecursos();
     super.initState();
   }
 
@@ -172,9 +270,6 @@ class _CreateResourceScreenState extends State<CreateResourceScreen> {
         icon: const Icon(Icons.add),
         onPressed: (){ 
           agregarRecurso();
-          nombreController.clear();
-          descripcionController.clear();
-          linksController.clear(); 
         },
       ),
       body: ReorderableListView(
@@ -197,14 +292,17 @@ class _CreateResourceScreenState extends State<CreateResourceScreen> {
           ),
         ]),
         children: [
-          for (final tile in myTiles)
+          for (final MapEntry<int, dynamic> tile in recursos.entries)
             Padding(
               key: ValueKey(tile),
               padding: const EdgeInsets.all(0),
               child: ResourceCard(
-                nombreRecurso: tile.toString(),
+                nombreRecurso: tile.value.toString(),
                 edit: true,
                 onDelete: borrarRecurso,
+                onTap: (){ 
+                  editarRecurso(tile.key); 
+                }
               ),
             ),
         ],
