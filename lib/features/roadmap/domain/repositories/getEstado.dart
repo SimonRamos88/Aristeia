c6f0350b27381916003358c467be76936f35a4ca
jaim3s <jujaimesc@unal.edu.co>
2023-05-13 21:30:32 -0500

    Funcion getEstado actualizada

--- lib/features/roadmap/domain/repositories/getEstado.dart
+++ lib/features/roadmap/domain/repositories/getEstado.dart
@@ -4,15 +4,21 @@ Future<int> getEstado(String? roadmapID) async {
   final querySnapshot =
   await FirebaseFirestore.instance.collection('roadmap').doc(roadmapID).collection('bloques').get();
   int bloquesCompletados = 0;
+  int bloquesSinIniciar = 0;
 
   // Contar los bloques completados
   querySnapshot.docs.forEach((doc) {
-    if (doc['estado'] > 1) bloquesCompletados++;
-  });
+    if (doc['estado'] > 1){
+      bloquesCompletados++;
+    }    else if (doc['estado'] == 0){
+      bloquesSinIniciar++;
+    }  
+    }
+  );
 
   if (bloquesCompletados == querySnapshot.size){ // Roadmap terminado
     return 2;
-  }else if (bloquesCompletados == 0){ // Roadmap sin iniciar
+  }else if (bloquesSinIniciar == querySnapshot.size){ // Roadmap sin iniciar
     return 0;
   }else{
     return 1; // Roadmap en progreso
